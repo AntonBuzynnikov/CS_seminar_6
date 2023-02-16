0a7db2d5a939cf8aec78d9bd5bb0c9cd1da15231
Buzynnikov <buzynnikov2111@gmail.com>
2023-02-16 08:04:01 +0700

    Complete Example061

---
+++ Example061/Program.cs
@@ -0,0 +1,41 @@
+﻿// 61. Показать двумерный массив размером m×n заполненный вещественными случайными числами
+
+System.Console.WriteLine("Введите количество строк: ");
+int row = Int32.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите количество столбцов: ");
+int column = Int32.Parse(Console.ReadLine());
+
+double[,] array2D = Random2DArray(row,column);
+Print2DArray(array2D);
+
+
+
+
+
+
+
+
+double[,] Random2DArray(int row, int col)
+{
+    double[,] arr = new double[row,col];
+    for(int i = 0; i<arr.GetLength(0); i++)
+    {
+        for(int j = 0; j<arr.GetLength(1);j++)
+        {
+            arr[i,j] = new Random().NextDouble();
+        }
+    }
+    return arr;
+}
+
+void Print2DArray(double[,] arr)
+{
+    for(int i = 0; i<arr.GetLength(0); i++)
+    {
+        for(int j = 0; j<arr.GetLength(1);j++)
+        {
+            System.Console.Write($"{arr[i,j],6:F2}");
+        }
+        System.Console.WriteLine();
+    }
+}
